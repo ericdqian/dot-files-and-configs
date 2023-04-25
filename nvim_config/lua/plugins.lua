9742a6f43de4d020a37410580c47d9f634ed428d
Eric <eric.qian.d@gmail.com>
2023-04-25 08:26:59 -0700

    prevent lsp from highlighitng

--- nvim_config/lua/plugins.lua
+++ nvim_config/lua/plugins.lua
@@ -306,11 +306,16 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local lspconfig = require('lspconfig')
 
+local custom_attach = function(client, bufnr) 
+    -- Do this to prevent lsp from overriding treesitter
+    client.server_capabilities.semanticTokensProvider = nil
+end
+
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
 local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'lua_ls' }
 for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup {
-        -- on_attach = my_custom_on_attach,
+        on_attach = custom_attach,
         capabilities = capabilities,
     }
 end

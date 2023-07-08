b167ec63c1d83b4366adf4f162c881d28b26d716
Eric <eric.qian.d@gmail.com>
2023-07-07 22:13:53 -0700

    move cmp and treesitter

--- nvim_config/lua/plugins.lua
+++ nvim_config/lua/plugins.lua
@@ -69,10 +69,14 @@ require('packer').startup(function(use)
 
     -- LSP plugins
     use('neovim/nvim-lspconfig')    -- Collection of common configurations for Nvim LSP
-    use('hrsh7th/nvim-cmp')         -- Autocompletion plugin
-    use('hrsh7th/cmp-nvim-lsp')     -- LSP source for nvim-cmp (where to get autocompletion suggestions)
     use('L3MON4D3/LuaSnip')         -- Snippets plugin
     use('saadparwaiz1/cmp_luasnip') -- Snippets source for nvim-cmp (where to get snippets suggestions)
+    use('hrsh7th/cmp-nvim-lsp')     -- LSP source for nvim-cmp (where to get autocompletion suggestions)
+    use{ 'hrsh7th/nvim-cmp',
+        config = function ()
+           require("config.cmp").setup()
+        end
+    }         -- Autocompletion plugin
 
 
     -- Gives diagnostics, pretty popups for goto definition/references/type
@@ -238,9 +242,11 @@ require('packer').startup(function(use)
         end
     }
     -- For text coloring
-    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate all' }
-
-
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate all',
+        config = function ()
+            require("config.treesitter").setup()
+        end
+    }
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
@@ -253,48 +259,6 @@ end)
 
 
 
-
-require 'nvim-treesitter.configs'.setup {
-    -- A list of parser names, or "all" (the four listed parsers should always be installed)
-    ensure_installed = { "javascript", "typescript", "python", "rust", "lua", "vim", "markdown",
-        "markdown_inline" },
-
-    -- Install parsers synchronously (only applied to `ensure_installed`)
-    sync_install = false,
-
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-    auto_install = true,
-
-    -- List of parsers to ignore installing (for "all")
-    --ignore_install = { "javascript" },
-
-    ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-    -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-    highlight = {
-        -- `false` will disable the whole extension
-        enable = true,
-        -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-        -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-        -- the name of the parser)
-        -- list of language that will be disabled
-        --disable = { "c", "rust" },
-        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-        disable = function(lang, buf)
-            local max_filesize = 500 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-                return true
-            end
-        end,
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-    },
-}
 
 
 vim.cmd([[
@@ -325,51 +289,6 @@ end
 
 vim.diagnostic.get(0, { update_in_insert = true });
 vim.diagnostic.get(0, { virtual_text = false });
-
--- luasnip setup
-local luasnip = require 'luasnip'
-
--- nvim-cmp setup
-local cmp = require 'cmp'
-cmp.setup {
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end,
-    },
-    mapping = cmp.mapping.preset.insert({
-        ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
-        ['<C-d>'] = cmp.mapping.scroll_docs(4),  -- Down
-        -- C-b (back) C-f (forward) for snippet placeholder navigation.
-        --['<C-Space>'] = cmp.mapping.complete(),
-        ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-        },
-        ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-    }),
-    sources = {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-    },
-}
 
 
 

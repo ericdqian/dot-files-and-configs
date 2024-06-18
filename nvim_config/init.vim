9380eb2ac520f9194af0570f1e4fde10e5a6ed48
Eric Qian <eric.qian.d@gmail.com>
2024-06-18 00:01:19 -0700

    much smoother vscode<>nvim experience by using vscode module with lua bindings

--- nvim_config/init.vim
+++ nvim_config/init.vim
@@ -2,6 +2,7 @@ runtime basic.vim
 runtime extended.vim
 runtime other.vim
 if exists('g:vscode')
+    lua require('vscode-bindings')
 else
     lua require('plugins')
 endif
---
+++ nvim_config/lua/vscode-bindings.lua
@@ -0,0 +1,82 @@
+vim.api.nvim_set_keymap(
+    "n",
+    "gr",
+    ":lua require('vscode-neovim').action('editor.action.goToReferences')<CR>",
+    { noremap = true, silent = true }
+)
+
+vim.api.nvim_set_keymap(
+    "n",
+    "<leader>rn",
+    ":lua require('vscode-neovim').action('editor.action.rename')<CR>",
+    { noremap = true, silent = true }
+)
+
+vim.api.nvim_set_keymap(
+    "n",
+    "-",
+    ":lua require('vscode-neovim').action('workbench.view.explorer')<CR>",
+    { noremap = true, silent = true }
+)
+
+vim.api.nvim_set_keymap(
+    "n",
+    "-",
+    ":lua require('vscode-neovim').action('workbench.view.explorer')<CR>",
+    { noremap = true, silent = true }
+)
+
+vim.api.nvim_set_keymap(
+    "n",
+    "<leader>a",
+    ":lua require('vscode-neovim').action('workbench.action.findInFiles')<CR>",
+    { noremap = true, silent = true }
+)
+
+vim.api.nvim_set_keymap(
+    "n",
+    "s",
+    ":lua require('vscode-neovim').action('leap.findForward')<CR>",
+    { noremap = true, silent = true }
+)
+
+vim.api.nvim_set_keymap(
+    "n",
+    "S",
+    ":lua require('vscode-neovim').action('leap.findBackward')<CR>",
+    { noremap = true, silent = true }
+)
+
+local comment_shortcuts = { "<leader>gb", "<leader>gl" }
+
+function visual_comment_selected_lines()
+    local vscode = require("vscode-neovim")
+    local start_line = vim.fn.line("'<")
+    local end_line = vim.fn.line("'>")
+
+    if end_line < start_line then
+        start_line = end_line
+        end_line = start_line - 1
+    end
+
+    local range = { start_line - 1, end_line - 1 }
+
+    vscode.action("editor.action.commentLine", { range = range })
+end
+
+for _, shortcut in ipairs(comment_shortcuts) do
+    vim.api.nvim_set_keymap("v", shortcut, ":lua visual_comment_selected_lines()<CR>", {})
+end
+
+function normal_comment_selected_lines()
+    local vscode = require("vscode-neovim")
+    local curr_line = vim.fn.line(".") - 1
+
+    local range = { curr_line, curr_line }
+
+    vscode.action("editor.action.commentLine", { range = range })
+end
+
+for _, shortcut in ipairs(comment_shortcuts) do
+    vim.api.nvim_set_keymap("n", shortcut, ":lua normal_comment_selected_lines()<CR>", {})
+end

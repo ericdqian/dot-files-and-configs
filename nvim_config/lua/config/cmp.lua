51bbf0ac8a7f0f290f8ac333b7e83f6c70fc8d57
Eric Qian <eric.qian.d@gmail.com>
2024-06-24 22:42:36 -0700

    dont collide with copilot

--- nvim_config/lua/config/cmp.lua
+++ nvim_config/lua/config/cmp.lua
@@ -18,7 +18,7 @@ function M.setup()
 				behavior = cmp.ConfirmBehavior.Replace,
 				select = true,
 			}),
-			["<Tab>"] = cmp.mapping(function(fallback)
+			["<C-n>"] = cmp.mapping(function(fallback)
 				if cmp.visible() then
 					cmp.select_next_item()
 				elseif luasnip.expand_or_jumpable() then
@@ -27,7 +27,7 @@ function M.setup()
 					fallback()
 				end
 			end, { "i", "s" }),
-			["<S-Tab>"] = cmp.mapping(function(fallback)
+			["<C-p>"] = cmp.mapping(function(fallback)
 				if cmp.visible() then
 					cmp.select_prev_item()
 				elseif luasnip.jumpable(-1) then

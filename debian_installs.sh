fb462981f45da58ff1a60bc8f7ee233affe3a3f0
Eric Qian <eric.qian.d@gmail.com>
2024-08-01 19:31:37 +0000

    better debian setup

--- debian_installs.sh
+++ debian_installs.sh
@@ -11,6 +11,8 @@ sudo apt install unzip
 
 # requierd for copilot
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm install 20
 
 
--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ echo Symlinking configs at $CUR_DIR
 ln -s ${CUR_DIR}/zsh_config/device_specific_zsh_config ~/.zshrc
 ln -s ${CUR_DIR}/zsh_config/general_zsh_config ~/.general_zsh_config
 # TODO: backup and overwrite ~/.config/nvim if it already exists so the symlink is created properly instead of making nvim_config inside of ~/.config/nvim
+mkdir -p ~/.config
 ln -s ${CUR_DIR}/nvim_config ~/.config/nvim
 mkdir -p ~/.config/kitty
 ln -s ${CUR_DIR}/kitty_config/kitty.conf ~/.config/kitty/kitty.conf

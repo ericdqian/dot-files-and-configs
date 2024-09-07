8cf82af47925992fdb4f105b760f039f5a16a9a7
Eric <eric.qian.d@gmail.com>
2024-09-07 09:01:50 -0700

    conditional installs during setup

--- debian_installs.sh
+++ debian_installs.sh
@@ -10,50 +10,103 @@ sudo apt install npm
 sudo apt install unzip
 
 # requierd for copilot
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-nvm install 20
+# NVM (Node Version Manager)
+if [ ! -d "$HOME/.nvm" ]; then
+    echo "Installing NVM..."
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
+    if command -v nvm &> /dev/null; then
+        echo "Installing Node.js v20..."
+        nvm install 20
+    else
+        echo "NVM installation failed. Please check and retry."
+    fi
+else
+    echo "NVM is already installed."
+fi
 
+# Create .local/bin directory
 mkdir -p ~/.local/bin
 
 # zsh
-sudo apt install zsh
+if ! command -v zsh &> /dev/null; then
+    echo "Installing zsh..."
+    sudo apt install zsh -y
+else
+    echo "zsh is already installed."
+fi
 
 # Neovim
-pushd ~/
-curl -LO https://github.com/neovim/neovim/releases/download/v0.9.1/nvim-linux64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
-popd
+if [ ! -d "/opt/nvim-linux64" ]; then
+    echo "Installing Neovim..."
+    pushd ~/
+    curl -LO https://github.com/neovim/neovim/releases/download/v0.9.1/nvim-linux64.tar.gz
+    sudo rm -rf /opt/nvim
+    sudo tar -C /opt -xzf nvim-linux64.tar.gz
+    rm nvim-linux64.tar.gz
+    popd
+    grep -qxF 'export PATH="$PATH:/opt/nvim-linux64/bin"' ~/.zshrc || echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.zshrc
+else
+    echo "Neovim is already installed."
+fi
 
-grep -qxF 'export PATH="$PATH:/opt/nvim-linux64/bin"' ~/.zshrc || echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.zshrc
-
-# Powerlink
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-
+# Powerlevel10k
+if [ ! -d ~/powerlevel10k ]; then
+    echo "Installing Powerlevel10k..."
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+else
+    echo "Powerlevel10k is already installed."
+fi
 # TODO: link .p10k
 
 # FZF
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --no-update-rc --completion --key-bindings
+if [ ! -d ~/.fzf ]; then
+    echo "Installing FZF..."
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install --no-update-rc --completion --key-bindings
+else
+    echo "FZF is already installed."
+fi
 
 # bat
-sudo apt install bat
-# https://github.com/sharkdp/bat/issues/982
-ln -s /usr/bin/batcat ~/.local/bin/bat
+if ! command -v bat &> /dev/null; then
+    echo "Installing bat..."
+    sudo apt install bat -y
+    # https://github.com/sharkdp/bat/issues/982
+    ln -s /usr/bin/batcat ~/.local/bin/bat
+else
+    echo "bat is already installed."
+fi
 
 # zsh highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-syntax-highlighting
+if [ ! -d ~/zsh-syntax-highlighting ]; then
+    echo "Installing zsh-syntax-highlighting..."
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-syntax-highlighting
+else
+    echo "zsh-syntax-highlighting is already installed."
+fi
 
 # ripgrep
-sudo apt-get install ripgrep
+if ! command -v rg &> /dev/null; then
+    echo "Installing ripgrep..."
+    sudo apt-get install ripgrep -y
+else
+    echo "ripgrep is already installed."
+fi
 
 # Oh-my-zsh
-# Keep oh-my-zsh last since if it runs zsh (controlled by RUNZSH), then it will stop the rest of the installation in this script
-if [[ "$SHELL" == */zsh ]]; then
-    export RUNZSH="no"
+if [ ! -d ~/.oh-my-zsh ]; then
+    echo "Installing Oh-my-zsh..."
+    # Keep oh-my-zsh last since if it runs zsh (controlled by RUNZSH), then it will stop the rest of the installation in this script
+    if [[ "$SHELL" == */zsh ]]; then
+        export RUNZSH="no"
+    fi
+    export KEEP_ZSHRC="yes"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+    echo "Oh-my-zsh is already installed."
 fi
-export KEEP_ZSHRC="yes"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Script completed. Please restart your terminal or run 'source ~/.zshrc' to apply changes."
--- mac_installs.sh
+++ mac_installs.sh
@@ -3,43 +3,86 @@
 
 mkdir -p ~/.local/bin
 
-# Install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-grep -qxF 'eval "$(/opt/homebrew/bin/brew shellenv)"' ~/.zshrc || echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zshrc
-
+# Install homebrew if not already installed
+if ! command -v brew &> /dev/null; then
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    grep -qxF 'eval "$(/usr/local/bin/brew)"' ~/.zshrc || echo 'eval "$(/usr/local/bin/brew)"' >> ~/.zshrc
+else
+    echo "Homebrew is already installed."
+fi
 
 # Neovim
-pushd ~/
-curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-x86_64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar xzf nvim-macos-x86_64.tar.gz -C /opt
-popd
-
-grep -qxF 'export PATH="$PATH:/opt/nvim-macos-x86_64/bin"' ~/.zshrc || echo 'export PATH="$PATH:/opt/nvim-macos-x86_64/bin"' >> ~/.zshrc
+if [ ! -d "/opt/nvim-macos-x86_64" ]; then
+    echo "Installing Neovim..."
+    pushd ~/
+    curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-x86_64.tar.gz
+    sudo rm -rf /opt/nvim
+    sudo tar xzf nvim-macos-x86_64.tar.gz -C /opt
+    rm nvim-macos-x86_64.tar.gz
+    popd
+    grep -qxF 'export PATH="$PATH:/opt/nvim-macos-x86_64/bin"' ~/.zshrc || echo 'export PATH="$PATH:/opt/nvim-macos-x86_64/bin"' >> ~/.zshrc
+else
+    echo "Neovim is already installed."
+fi
 
 # Powerlink
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-
+if [ ! -d ~/powerlevel10k ]; then
+    echo "Installing Powerlevel10k..."
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+else
+    echo "Powerlevel10k is already installed."
+fi
 # TODO: link .p10k
 
 # FZF
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --no-update-rc --completion --key-bindings
+if [ ! -d ~/.fzf ]; then
+    # https://github.com/junegunn/fzf?tab=readme-ov-file#upgrading-fzf
+    echo "Installing FZF..."
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install --no-update-rc --completion --key-bindings
+else
+    echo "FZF is already installed."
+fi
 
 # bat
-brew install bat
+if ! command -v bat &> /dev/null; then
+    echo "Installing bat..."
+    brew install bat
+else
+    echo "bat is already installed."
+fi
 
 # zsh highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-syntax-highlighting
+if [ ! -d ~/zsh-syntax-highlighting ]; then
+    echo "Installing zsh-syntax-highlighting..."
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-syntax-highlighting
+else
+    echo "zsh-syntax-highlighting is already installed."
+fi
 
 # ripgrep
-brew install ripgrep
+if ! command -v rg &> /dev/null; then
+    echo "Installing ripgrep..."
+    brew install ripgrep
+else
+    echo "ripgrep is already installed."
+fi
 
-#k9s
-brew install derailed/k9s/k9s
-
-# # Install Poetry
-# curl -sSL https://install.python-poetry.org | POETRY_HOME=~/.bin/poetry python3
+# k9s
+if ! command -v k9s &> /dev/null; then
+    echo "Installing k9s..."
+    brew install derailed/k9s/k9s
+else
+    echo "k9s is already installed."
+fi
 
 # Oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
+if [ ! -d ~/.oh-my-zsh ]; then
+    echo "Installing Oh-my-zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
+else
+    echo "Oh-my-zsh is already installed."
+fi
+
+echo "Script completed. Please restart your terminal or run 'source ~/.zshrc' to apply changes."
